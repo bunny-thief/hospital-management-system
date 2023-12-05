33455d5e2b038a95787552b358419d4577cfffd7
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2023-12-05 13:51:23 +0100

    Add Doctor class

---
+++ Doctor.java
@@ -0,0 +1,43 @@
+import java.util.Objects;
+
+public class Doctor {
+
+    private static int doctorID = 0;
+    private int id;
+    private String name;
+
+    public Doctor(String name) {
+        id = ++doctorID;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Doctor doctor = (Doctor) o;
+        return getId() == doctor.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+}
