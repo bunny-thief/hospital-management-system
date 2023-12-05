ac30c6c4b3da812f1b8bc8708053101f48416dd5
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2023-12-05 15:00:12 +0100

    Add PatientRegistration class

---
+++ PatientRegistration.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class PatientRegistration {
+
+    private Set<Patient> patients;
+    private int patientId = 1;
+
+    public PatientRegistration() {
+        patients = new HashSet<Patient>();
+    }
+
+    public int idGenerator() {
+        return patients.isEmpty() ? patientId : patientId + 1;
+    }
+
+    public Patient createPatient(String name, int age, String gender, String contact) {
+        return new Patient(idGenerator(), name, age, gender, contact);
+    }
+
+    public void registering(Patient patient) {
+        patients.add(patient);
+        System.out.println();
+    }
+
+}
