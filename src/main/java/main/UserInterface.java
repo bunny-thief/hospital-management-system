e53ef290b3858e448d73e4b00a657b8c5549b04b
Calton Manhique <102233854+CaltonManhique@users.noreply.github.com>
2023-12-05 16:20:05 +0100

    Update UserInterface.java

--- src/main/java/main/UserInterface.java
+++ src/main/java/main/UserInterface.java
@@ -1,18 +1,41 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class UserInterface {
 
 	private Scanner scanner = new Scanner(System.in);
 	private PatientRegistration patientRegistration = new PatientRegistration();
+	private List<Appointment> appointments = new ArrayList<>();
+
+	public void makeAppointment(String name, Doctor doctor) {
+
+		Set<Patient> arr = patientRegistration.getPatients();
+
+		Patient patient = arr.stream().filter(n -> n.getName().equalsIgnoreCase(name)).findFirst().orElse(null);
+
+		if (patient == null) {
+			System.out.println("Patient not found!");
+		} else {
+			Appointment appointment = new Appointment(doctor, patient);
+			appointments.add(appointment);
+			System.out.println(appointment.toString());
+		}
+	}
+
+	public Doctor createDoctor(String name) {
+		return new Doctor(name);
+	}
 
 	public void userInteface() {
 
 		int option = 0;
 		do {
 
-			System.out.println("1. Patient registration\n2. Appointment scheduling\n3.View information\n4. Exit");
+			System.out.println("1. Patient registration\n2. Appointment scheduling\n3. View information\n4. Exit");
 
 			option = scanner.nextInt();
 
@@ -35,7 +58,15 @@ public class UserInterface {
 				patientRegistration.registering(patient);
 			}
 
-			case 2 -> System.out.println("Appointment");
+			case 2 -> {
+				System.out.println("Enter patient name:");
+				String patientName = scanner.nextLine();
+
+				System.out.println("Enter doctor name:");
+				String doctorName = scanner.nextLine();
+
+				makeAppointment(patientName, createDoctor(doctorName));
+			}
 
 			case 3 -> patientRegistration.getPatients().forEach(System.out::println);
 
