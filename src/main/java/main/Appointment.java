e356aacaa36a56ebaf38c9ecbac2449682463214
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2023-12-05 15:10:16 +0100

    Change directory structure
    Placed all class files in src/main/java/main

--- src/main/java/main/Appointment.java
+++ src/main/java/main/Appointment.java
@@ -1,3 +1,5 @@
+package main;
+
 public class Appointment {
     private Doctor doctor;
     private Patient patient;
@@ -11,5 +13,5 @@ public class Appointment {
     public String toString() {
         return String.format("%s has an appointment with %s", patient.getName(), doctor.getName());
     }
-    
+
 }
